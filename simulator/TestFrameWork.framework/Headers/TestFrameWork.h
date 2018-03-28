66c6d191641ec37c1bd9a58b1775e7da816379fe
zengzhihui <zengzhihui@bytedance.com>
2018-03-28 19:27:36 +0800

    simulator

---
+++ simulator/TestFrameWork.framework/Headers/TestFrameWork.h
@@ -0,0 +1,19 @@
+//
+//  TestFrameWork.h
+//  TestFrameWork
+//
+//  Created by zengzhihui on 2018/3/26.
+//  Copyright © 2018年 zengzhihui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestFrameWork.
+FOUNDATION_EXPORT double TestFrameWorkVersionNumber;
+
+//! Project version string for TestFrameWork.
+FOUNDATION_EXPORT const unsigned char TestFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestFrameWork/PublicHeader.h>
+#import "TestObject.h"
+
